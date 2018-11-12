914be237677f7d5c9754e65ed0dfe077834ccd88
crndolo <ndoloraphael@gmail.com>
2018-11-12 10:55:27 +0300

    Second commit. Template files for viewing all entries in the database

--- church_admin/urls.py
+++ church_admin/urls.py
@@ -3,7 +3,25 @@ from church_admin import views
 
 urlpatterns = [
 	#/church_admin/
-    url(r'^$', views.home, name='admin-home')
+    url(r'^$', views.index, name='admin-home'),
+    #/church_admin/event
+    url(r'^event/$', views.events_view_all, name='all-events'),
+    #/church_admin/income
+    url(r'^income/$', views.incomes_view_all, name='all-incomes'),
+    #/church_admin/expense
+    url(r'^expense/$', views.expenses_view_all, name='all-expenses'),
+    #/church_admin/service
+    url(r'^service/$', views.services_view_all, name='all-services'),
+    #/church_admin/ministry
+    url(r'^ministry/$', views.ministries_view_all, name='all-ministries'),   
+    #/church_admin/contribution
+    url(r'^contribution/$', views.contributions_view_all, name='all-contributions'), 
+    #/church_admin/pastoral_group
+    url(r'^pastoral_group/$', views.pastoral_groups_view_all, name='all-pastoral-groups'),    
+    #/church_admin/church_attendance
+    url(r'^church_attendance/$', views.church_attendances_view_all, name='all-church-attendances'),
+    #/church_admin/contribution_type
+    url(r'^contribution_type/$', views.contribution_types_view_all, name='all-contribution-types'),
 ]
 
 '''
--- church_admin/views.py
+++ church_admin/views.py
@@ -1,9 +1,62 @@
 from django.template import loader
 from django.shortcuts import render 
 from django.http import HttpResponse
+from church_admin.models import Event
+from church_admin.models import Income
+from church_admin.models import Expense
+from church_admin.models import Service
+from church_admin.models import Ministry
+from church_admin.models import Contribution
+from church_admin.models import PastoralGroup
+from church_admin.models import ChurchAttendance
+from church_admin.models import ContributionType
 
 # Create your views here.
 
-def home(request):
-	#return HttpResponse("Home Page")
+def index(request):
 	return render(request, 'church_admin/index.html')
+
+def events_view_all(request):
+	all_events = Event.objects.all()
+	context = {'all_events': all_events}
+	return render(request, 'church_admin/all_events.html', context)
+
+def incomes_view_all(request):
+	all_incomes = Income.objects.all()
+	context = {'all_incomes': all_incomes}
+	return render(request, 'church_admin/all_incomes.html', context)
+
+def expenses_view_all(request):
+	all_expenses = Expense.objects.all()
+	context = {'all_expenses': all_expenses}
+	return render(request, 'church_admin/all_expenses.html', context)
+
+def services_view_all(request):
+	all_services = Service.objects.all()
+	context = {'all_services': all_services}
+	return render(request, 'church_admin/all_services.html', context)
+
+def ministries_view_all(request):
+	all_ministries = Ministry.objects.all()
+	context = {'all_ministries': all_ministries}
+	return render(request, 'church_admin/all_ministries.html', context)
+
+def contributions_view_all(request):
+	all_contributions = Contribution.objects.all()
+	context = {'all_contributions': all_contributions}
+	return render(request, 'church_admin/all_contributions.html', context)
+
+def pastoral_groups_view_all(request):
+	all_pastoral_groups = PastoralGroup.objects.all()
+	context = {'all_pastoral_groups': all_pastoral_groups}
+	return render(request, 'church_admin/all_pastoral_groups.html', context)
+
+def church_attendances_view_all(request):
+	all_church_attendances = ChurchAttendance.objects.all()
+	context = {'all_church_attendances': all_church_attendances}
+	return render(request, 'church_admin/all_church_attendance.html', context)
+
+def contribution_types_view_all(request):
+	all_contribution_types = ContributionType.objects.all()
+	context = {'all_contribution_types': all_contribution_types}
+	return render(request, 'church_admin/all_contribution_types.html', context)
